5ca0148b35fe4566a137e0e4279fbed6e11ae906
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-09-21 04:03:19 +0300

    add  prev and next func

--- js/gallery.js
+++ js/gallery.js
@@ -86,30 +86,78 @@ galery.insertAdjacentHTML("beforeend", galeryItems);
 document.querySelector(".gallery").onclick = (e) => {
   e.preventDefault();
   if (e.currentTarget != e.target) {
-    const galeryImage = basicLightbox.create(
-      `
-          <span class="counter">${e.target.dataset.id}/${images.length}</span>
-          <span class="back-btn">Back</span>
-          <span class="close-btn"></span>
-          <span class="next-btn">Next</span>
-          <img class="galery-image" width="1400" height="900" src="${e.target.dataset.source}">
-          <p class="image-description">${e.target.alt}</p>
-          
-        `,
-      {
-        className: "bg-overlay image-container",
-        closable: false,
-      }
-    );
-    galeryImage.show(() => {
-      setTimeout(() => {
-        document.querySelector(".image-description").classList.add("is-open");
-      }, 300);
-    });
-
-    const button = document.querySelector(".close-btn");
-    button.addEventListener("click", function () {
-      galeryImage.close();
-    });
+    displayImage(e.target);
   }
 };
+
+function displayImage(imageEl) {
+  function closeGalery() {
+    galeryImage.close();
+  }
+
+  function getElement(elId, isNext) {
+    let elIdInt = Number(elId);
+    const len = images.length;
+    if (elIdInt % len === 0 && isNext) {
+      elIdInt = 0;
+    }
+    if (elIdInt === 1 && !isNext) {
+      elIdInt = len + 1;
+    }
+    let newElId = isNext ? elIdInt + 1 : elIdInt - 1;
+    console.log("newID:", newElId);
+    const tragetElment = document.querySelector(`[data-id="${newElId}"]`);
+    return tragetElment;
+  }
+  function showNext() {
+    closeGalery();
+    const next = getElement(imageEl.dataset.id, true);
+    displayImage(next);
+  }
+
+  function showPrev() {
+    closeGalery();
+    const prev = getElement(imageEl.dataset.id, false);
+    displayImage(prev);
+  }
+
+  const galeryImage = basicLightbox.create(
+    `
+      <span class="counter">${imageEl.dataset.id}/${images.length}</span>
+      <span class="back-btn js-galery-back-${imageEl.dataset.id}"></span>
+      <span class="close-btn js-galery-close-${imageEl.dataset.id}"></span>
+      <span class="next-btn js-galery-next-${imageEl.dataset.id}"></span>
+      <img class="galery-image" width="1400" height="900" src="${imageEl.dataset.source}">
+      <p class="image-description js-galery-desc-${imageEl.dataset.id}">${imageEl.alt}</p>
+    `,
+    {
+      className: "bg-overlay image-container",
+      onClose: () => {
+        closeButton.removeEventListener("click", closeGalery);
+        nextButton.removeEventListener("click", showNext);
+        nextButton.removeEventListener("click", showPrev);
+      },
+    }
+  );
+  galeryImage.show(() => {
+    setTimeout(() => {
+      document
+        .querySelector(`.js-galery-desc-${imageEl.dataset.id}`)
+        .classList.add("is-open");
+    }, 300);
+  });
+  // Get buttons
+  const closeButton = document.querySelector(
+    `.js-galery-close-${imageEl.dataset.id}`
+  );
+  const nextButton = document.querySelector(
+    `.js-galery-next-${imageEl.dataset.id}`
+  );
+  const backButton = document.querySelector(
+    `.js-galery-back-${imageEl.dataset.id}`
+  );
+  // add event Listener for every button
+  closeButton.addEventListener("click", closeGalery);
+  nextButton.addEventListener("click", showNext);
+  backButton.addEventListener("click", showPrev);
+}
